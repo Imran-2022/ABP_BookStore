b1aeb1ef457c21e3e7c15eb05f0807637219acb5
Imran-2022 <mdimranulhaque.hstu@gmail.com>
2025-12-03 01:38:15 +0600

    8.authors application layer

--- src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
+++ src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
@@ -3,7 +3,14 @@ namespace Acme.BookStore.Permissions;
 public static class BookStorePermissions
 {
     public const string GroupName = "BookStore";
-
+    // *** ADDED a NEW NESTED CLASS ***
+    public static class Authors
+    {
+        public const string Default = GroupName + ".Authors";
+        public const string Create = Default + ".Create";
+        public const string Edit = Default + ".Edit";
+        public const string Delete = Default + ".Delete";
+    }
     public static class Dashboard
         {
             public const string Host = GroupName + ".Dashboard.Host";
